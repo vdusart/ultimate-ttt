00f613582400357ad75261f78fd14ca4e79a82de
Victor DUSART <dusartvictor@gmail.com>
2024-07-05 01:07:44 +0200

    Take the alphabet list out of the function

--- backend/src/utils.rs
+++ backend/src/utils.rs
@@ -1,8 +1,9 @@
 use nanoid::nanoid;
 
+const ALPHABET: [char; 16] = [
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f'
+];
+
 pub fn generate_id() -> String {
-    let alphabet: [char; 16] = [
-        '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f'
-    ];
-    nanoid!(10, &alphabet)
+    nanoid!(10, &ALPHABET)
 }
