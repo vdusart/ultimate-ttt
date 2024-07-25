544430b9ba4c1b4c29fa4908e0c5f7e98aa1bb93
Victor DUSART <dusartvictor@gmail.com>
2024-07-25 22:06:28 +0200

    End of sqlx migration

--- backend/src/api/game.rs
+++ backend/src/api/game.rs
@@ -1,14 +1,16 @@
-use actix_web::{get, post, web::{Json, Path}};
+use actix_web::{
+    get, post,
+    web::{Json, Path},
+};
 
-use crate::model::{game::Game, grid::{Cell, Grid}};
+use crate::model::game::Game;
 
 #[post("/game")]
 pub async fn create_game() -> Json<String> {
     let game = match Game::new(1).await {
         Ok(game) => game,
-        Err(error_msg) => todo!("Return an http error with the error_msg in it.")
+        Err(_error_msg) => todo!("Return an http error with the error_msg in it."),
     };
-    Game::count().await;
     Json(game.id)
 }
 
@@ -18,7 +20,7 @@ pub async fn get_game(path: Path<String>) -> Json<String> {
     println!("The requested id is: {}", game_id);
     let game = match Game::load(game_id).await {
         Ok(game) => game,
-        Err(error_msg) => todo!("Return an http error with the error_msg in it.")
+        Err(_error_msg) => todo!("Return an http error with the error_msg in it."),
     };
     Json(game.grid.export())
 }
